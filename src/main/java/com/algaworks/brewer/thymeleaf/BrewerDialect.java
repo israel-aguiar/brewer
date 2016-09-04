1af36d137a91268dcbae4e43679209acde96831c
israel <israel@metal>
2016-09-04 16:14:02 -0300

    13.3 - Novo elemento para mensagens

--- src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import com.algaworks.brewer.thymeleaf.processor.ClassForErrorAttributTagProcessor;
+import com.algaworks.brewer.thymeleaf.processor.MessageElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect{
 
@@ -19,6 +20,7 @@ public class BrewerDialect extends AbstractProcessorDialect{
 	public Set<IProcessor> getProcessors(String dialectPrefix) {
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributTagProcessor(dialectPrefix));
+		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
