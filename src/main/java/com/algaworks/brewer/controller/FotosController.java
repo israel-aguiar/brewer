a2fd77cddaff3ac6e3abdab2bd274e8ff85ba1d5
israel.aguiar <israel.aguiar@192.168.56.1>
2016-09-12 20:04:45 -0300

    14.8 - Mostrando a foto na tela

--- src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -1,6 +1,8 @@
 package com.algaworks.brewer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,5 +30,10 @@ public class FotosController {
 		thread.run();
 		
 		return resultado;
+	}
+	
+	@GetMapping("/temp/{nome:.*}")
+	public byte[] recuperarFotoTemporaria(@PathVariable String nome) {
+		return fotoStorage.recuperarFotoTemporaria(nome);
 	}
 }
