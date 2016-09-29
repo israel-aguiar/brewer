65ad071ebe13b99dabee9f85a98b12ce55239ff9
israel <israel@metal>
2016-09-29 18:31:37 -0300

    15.15 - Componente Thymeleaf para ordenação

--- src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/com/algaworks/brewer/thymeleaf/BrewerDialect.java
@@ -9,6 +9,7 @@ import org.thymeleaf.standard.StandardDialect;
 
 import com.algaworks.brewer.thymeleaf.processor.ClassForErrorAttributTagProcessor;
 import com.algaworks.brewer.thymeleaf.processor.MessageElementTagProcessor;
+import com.algaworks.brewer.thymeleaf.processor.OrderElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect{
 
@@ -21,6 +22,7 @@ public class BrewerDialect extends AbstractProcessorDialect{
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributTagProcessor(dialectPrefix));
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
+		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
