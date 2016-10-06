2389c91ced3e69c081c39bd8537ddfd081861268
israel.aguiar <israel.aguiar@192.168.56.1>
2016-10-06 19:21:45 -0300

    16.4 - Implementando radio TipoPessoa

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -1,33 +1,34 @@
 package com.algaworks.brewer.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.brewer.model.Cliente;
+import com.algaworks.brewer.model.TipoPessoa;
 
 @Controller
+@RequestMapping("/clientes")
 public class ClientesController {
 	
-	@RequestMapping("clientes/novo")
-	public String novo(Cliente cliente) {
-		return "cliente/CadastroCliente";
+	@RequestMapping("/novo")
+	public ModelAndView novo(Cliente cliente) {
+		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		
+		return mv;
 	}
 	
-	public String cadastrar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes attributes) {
-		
-		if (result.hasErrors()) {
-			result.getAllErrors().stream().forEach(e -> System.out.println(e));
-			return novo(cliente);
-		}
-		
-		
-		attributes.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso");
-		return "redirect:/clientes/novo";
-	}
+//	public String cadastrar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes attributes) {
+//		
+//		if (result.hasErrors()) {
+//			result.getAllErrors().stream().forEach(e -> System.out.println(e));
+//			return novo(cliente);
+//		}
+//		
+//		
+//		attributes.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso");
+//		return "redirect:/clientes/novo";
+//	}
 
 }
