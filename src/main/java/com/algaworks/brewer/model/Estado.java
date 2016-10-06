975c9caeb565fdada3300c646f18d9697c5f7a92
israel.aguiar <israel.aguiar@192.168.56.1>
2016-10-06 20:48:53 -0300

    16.6 - Aplicando migração estado e cidade

--- src/main/java/com/algaworks/brewer/model/Estado.java
+++ src/main/java/com/algaworks/brewer/model/Estado.java
@@ -1,5 +1,49 @@
 package com.algaworks.brewer.model;
 
-public class Estado {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+public class Estado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	private String sigla;
+	
+	public Estado() {
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
 }
