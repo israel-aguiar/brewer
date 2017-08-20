528d1e640cd39e04b8d713e226c6bd1edaee0658
israelba <israel.aguiar@gmail.com>
2017-08-20 18:07:05 -0300

    16.14. Desafio: Finalizar cadastro de cidade

--- src/main/java/com/algaworks/brewer/model/Cidade.java
+++ src/main/java/com/algaworks/brewer/model/Cidade.java
@@ -8,6 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,8 +22,10 @@ public class Cidade {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
 	
+	@NotNull(message = "Estado é obrigatório")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "codigo_estado")
 	@JsonIgnore
