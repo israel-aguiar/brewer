bd2bccbf66d533f9059c15af09c9f892af60a5a7
israelba <israel.aguiar@gmail.com>
2017-08-19 22:55:24 -0300

    16.13 - Finalizando pesquisa de clientes

--- src/main/java/com/algaworks/brewer/repository/helper/cliente/ClientesImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/cliente/ClientesImpl.java
@@ -8,6 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -29,12 +30,15 @@ public class ClientesImpl implements ClientesQueries {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	@Transactional
+	@Transactional(readOnly = true)
 	public Page<Cliente> filtrar(ClienteFilter filtro, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cliente.class);
 		
 		paginacaoUtil.preparar(criteria, pageable);
 		adicionarFiltro(filtro, criteria);
+		criteria.createAlias("endereco.cidade", "c", JoinType.LEFT_OUTER_JOIN);
+		criteria.createAlias("c.estado", "e", JoinType.LEFT_OUTER_JOIN);
+		
 		
 		return new PageImpl<>(criteria.list(), pageable, total(filtro));
 	}
