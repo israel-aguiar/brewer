109afe39eccf17faf70f23504e7256c98edcf055
israelba <israel.aguiar@gmail.com>
2017-08-22 09:48:17 -0300

    18.3 - Desafio: Começando o cadastro do usuário

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -3,24 +3,33 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Usuario;
 
 
 @Controller
+@RequestMapping("/usuarios")
 public class UsuariosController {
 	
-	@RequestMapping("usuarios/novo")
-	public String novo(Usuario usuario) {
-		return "usuario/CadastroUsuario";
+	@RequestMapping("/novo")
+	public ModelAndView novo(Usuario usuario) {
+		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
+		return mv;
 	}
 	
-	public String cadastrar(@Valid Usuario usuario, BindingResult result, Model model, RedirectAttributes attributes) {
-		return null;
+	@PostMapping("/novo")
+	public ModelAndView cadastrar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors())
+			return novo(usuario);
+		
+		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso.");
+		
+		return new ModelAndView("redirect:/usuarios/novo");
 	}
 
 }
