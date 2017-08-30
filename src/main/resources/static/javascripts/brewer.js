b8fcbf349acef5172bc3303be0a16169e1e6c5cf
israelba <israel.aguiar@gmail.com>
2017-08-29 21:37:16 -0300

    19.9 - Configurando o CSRF

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -74,6 +74,24 @@ Brewer.MaskDate = (function() {
 	return MaskDate;
 }());
 
+Brewer.Security = (function(){
+	
+	function Security() {
+		this.token = $('input[name=_csrf]').val();
+		this.header = $('input[name=_csrf_header').val();
+	}
+	
+	Security.prototype.enable = function() {
+		
+		$(document).ajaxSend(function(event, jqxhr, settings) {
+			jqxhr.setRequestHeader(this.header, this.token);
+		}.bind(this));
+	}
+	
+	return Security;
+	
+}());
+
 
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
@@ -87,4 +105,7 @@ $(function() {
 	
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
+	
+	var security = new Brewer.Security();
+	security.enable();
 });
