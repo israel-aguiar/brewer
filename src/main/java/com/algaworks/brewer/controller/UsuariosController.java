8f7404e670ca8e1dbdb98942698c8675ce8d5c4e
israelba <israel.aguiar@gmail.com>
2017-09-21 23:52:52 -0300

    21.3 - Trabalhando no filtro por grupo

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -65,7 +65,7 @@ public class UsuariosController {
 	public ModelAndView pesquisar(UsuarioFilter usuarioFilter) {
 		ModelAndView mv = new ModelAndView("usuario/PesquisaUsuarios");
 		
-		mv.addObject("usuarios", usuarios.findAll());
+		mv.addObject("usuarios", usuarios.filtrar(usuarioFilter));
 		mv.addObject("grupos", grupos.findAll());
 		
 		return mv;
